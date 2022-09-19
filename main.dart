4c5751affc48bcbc16c2120d649e267c71c89ebd
qws0708 <tjddnxodzm12@naver.com>
2022-09-19 18:54:48 +0900

    2022-09-19

--- main.dart
+++ main.dart
@@ -22,7 +22,7 @@ class MyHomePage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('시발'),
+        title: Text('ListView'),
       ),
 
       body: ListView(
