951a04b9635e5e10cf8c827e01d6131c103a38cc
qws0708 <112889935+qws0708@users.noreply.github.com>
2022-10-10 16:28:55 +0900

    일정 관리 App
    Signed-off-by: qws0708 <112889935+qws0708@users.noreply.github.com>

---
+++ main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class Todo{
+  bool isDone = false;
+  String title;
+
+  Todo(this.title);
+}
+
+class MyApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: '할 일 관리',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget{
+
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage>{
+
+  final _items = <Todo>[];
+
+  var _todoControoller = TextEditingController();
+
+  void dispose(){
+    _todoControoller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildItemWidget(Todo todo){
+    return ListTile(
+      onTap: () => _toggleTodo(todo),
+      title: Text(
+        todo.title,
+        style: todo.isDone
+            ?TextStyle(
+          decoration: TextDecoration.lineThrough,
+          fontStyle: FontStyle.italic,
+        )
+            :null,
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.delete_forever),
+        onPressed: () => _deleteTodo(todo),
+      ),
+    );
+  }
+
+  void _addTodo(Todo todo){
+    setState(() {
+      _items.add(todo);
+      _todoControoller.text = '';
+    });
+  }
+
+  void _deleteTodo(Todo todo){
+    setState(() {
+      _items.remove(todo);
+    });
+  }
+
+  void _toggleTodo(Todo todo){
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('남은 할 일'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller:_todoControoller,
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text('추가'),
+                  onPressed: () => _addTodo(Todo(_todoControoller.text)),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: _items.map((todo) => _buildItemWidget(todo)).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
