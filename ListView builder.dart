644e8ae9a883954909ac1df837dc6c8d10fe6839
qws0708 <112889935+qws0708@users.noreply.github.com>
2022-10-17 17:43:42 +0900

    ListView builder 실습
    Signed-off-by: qws0708 <112889935+qws0708@users.noreply.github.com>

---
+++ ListView builder.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ListViewPage(),
+    );
+  }
+}
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  var titleList = [
+    'Dentist',
+    'Pharmacist',
+    'School teacher',
+    'IT manager',
+    'Account',
+    'Layer',
+    'Hairdresser',
+    'Physician',
+    'Web developer',
+    'Medical Secretary'
+  ];
+
+  var imageList = [
+    'image/1.png',
+    'image/2.png',
+    'image/3.png',
+    'image/4.png',
+    'image/5.png',
+    'image/6.png',
+    'image/7.png',
+    'image/8.png',
+    'image/9.png',
+    'image/10.png',
+  ];
+
+  var description = [
+    '1.There are different types of careers you can pursue in your life. Which one will it be?',
+    '2.There are different types of careers you can pursue in your life. Which one will it be?',
+    '3.There are different types of careers you can pursue in your life. Which one will it be?',
+    '4.There are different types of careers you can pursue in your life. Which one will it be?',
+    '5.There are different types of careers you can pursue in your life. Which one will it be?',
+    '6.There are different types of careers you can pursue in your life. Which one will it be?',
+    '7.There are different types of careers you can pursue in your life. Which one will it be?',
+    '8.There are different types of careers you can pursue in your life. Which one will it be?',
+    '9.There are different types of careers you can pursue in your life. Which one will it be?',
+    '10.There are different types of careers you can pursue in your life. Which one will it be?',
+  ];
+
+  void showPopup(context, title, image, description) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 380,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.white),
+            child: Column(
+              children: [
+                ClipRRect(
+                  //이미지를 사각형 모양으로 출력해주는 윗젯
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    image,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    description,
+                    maxLines: 3, //최대 몇 줄까지 데이터를 나타낼지 지정
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[500],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton.icon(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text('close'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.6;
+    //반응형 앱을 만들기위해 화면의 방향이 바뀔때마다 rebuild해줘야 함, 여기서는 가로모드만 신경 쓰므로 width * 0.6
+    // => 디바이스가 가로모드로 전환 되었을 때 description data가 전체화면의 60%를 차지하도록 설정 함
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ListView',
+          style: TextStyle(color: Colors.grey),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: titleList.length, //보여지는 아이템의 갯수 지정
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              debugPrint(
+                  titleList[index]); //print에 노란 밑줄이 생기면 debugPrint로 대체해서 사용
+              showPopup(context, titleList[index], imageList[index],
+                  description[index]);
+            },
+            child: Card(
+              child: Row(
+                children: [
+                  SizedBox(
+                    //Container를 사용해도 무방하나 SizedBox는 크기를 지정해주는 기능 외에 다른 기능이 없어 더 효율적임
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imageList[index]),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          titleList[index],
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          width: width,
+                          child: Text(
+                            description[index],
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[500]),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
