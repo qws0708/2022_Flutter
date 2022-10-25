3b92d118baf57f8c531b1630f1aea97666774eb5
qws0708 <112889935+qws0708@users.noreply.github.com>
2022-10-25 19:40:05 +0900

    로그인과 주사위 게임 플러터 앱 만들기 Part1
    Signed-off-by: qws0708 <112889935+qws0708@users.noreply.github.com>

--- main.dart
+++ main.dart
@@ -1,27 +1,96 @@
 import 'package:flutter/material.dart';
-import 'mypage.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        textTheme: TextTheme(
-          bodyText2: TextStyle(
-            fontSize: 30,
-            color: Colors.white
-          )
-        )
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
+    );
+  }
+}
 
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
       ),
-      home: MyPage(),
+      body: SingleChildScrollView(  // 키보드가 밑에서 밀고 올라오면서 스크린 영역을 차지할 때 그 영역만큼 스크린을 스크롤 할 수 있도록 도와주는 위젯
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170.0,
+                height: 190.0,
+              ),
+            ),
+            //text 필드로 어떤 정보들을 입력받는 화면을 구성
+            Form(
+                child: Theme(
+                    data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: InputDecorationTheme(
+                            labelStyle:
+                                TextStyle(color: Colors.teal, fontSize: 15.0))),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration:
+                                InputDecoration(labelText: 'Enter "dice"'),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            decoration:
+                            InputDecoration(labelText: 'Enter Password'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,  //비밀번호 입력시 *로 가려줌
+                          ),
+
+                          SizedBox(height: 40.0),
+
+                          ButtonTheme(
+                            minWidth: 100.0,
+                              height: 50.0,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.orangeAccent
+                                ),
+                                child: Icon(
+                                    Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 35.0,
+                                ),
+                                onPressed: (){},
+                                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
