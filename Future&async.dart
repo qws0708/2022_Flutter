a22a575f5db94e17d3fa799efefdb3bcee896d27
qws0708 <112889935+qws0708@users.noreply.github.com>
2022-11-02 15:31:48 +0900

    Future&async 예제
    Signed-off-by: qws0708 <112889935+qws0708@users.noreply.github.com>

---
+++ Future&async.dart
@@ -0,0 +1,33 @@
+void main() async{
+
+  methodA();
+  await methodB();
+  await methodC('main');
+  methodD();
+}
+
+methodA(){
+  print('A');
+}
+
+methodB() async{
+  print('B starts');
+  await methodC('B');
+  print('B end');
+}
+
+methodC(String from) async{
+  print('C start from $from');
+
+  Future((){
+    print('C Running Future from $from');
+  }).then((_){
+    print('C end of Future from $from');
+  });
+
+  print('C end from $from');
+}
+
+methodD(){
+  print('D');
+}
